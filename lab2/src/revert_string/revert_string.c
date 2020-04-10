316cadbbb2680485d643a5f685e5fb23d079aec1
trynottofil <egorodintsov50@gmail.com>
2020-04-10 16:55:54 +0000

    добавлен

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,13 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int lenght=strlen(str);
+    char*newstring = (char*) calloc(lenght, sizeof(char));
+    int i=0;
+    for (i=lenght-1; i>-1;i--)
+    {
+        newstring[lenght-i-1]=str[i];
+    }
+    strcpy(str, newstring);
 }
 
